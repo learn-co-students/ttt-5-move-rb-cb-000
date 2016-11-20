39d13969c3235bbf33f3374aeff17fcd6a307851
Marcus Magnusson <marcusmagnusson@outlook.com>
2016-11-20 15:28:41 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,15 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+# converts a user_input to an integer
+# subtracts 1 from the input
+# returns -1 for strings without integers
+def input_to_index(input)
+  input.to_i - 1
+end
+
+# Accepts 3 arguments, board, position player wants to fill and their symbol, ,X or O
+# provides a default value for the 3rd argument
+def move(board, index, symbol = "X")
+  board[index] = symbol
+end
