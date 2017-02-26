63efb77e37ba1979aa32983680ede7e75ee01f00
Alex Segers <a.segers.97@gmail.com>
2017-02-26 23:59:08 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -9,7 +9,8 @@ end
 # code your input_to_index and move method here!
 
 def input_to_index(input)
-  input = input.to_i - 1
+  input = input.to_i
+  input -= 1
 end
 
 def move(board, index, player = "X")
