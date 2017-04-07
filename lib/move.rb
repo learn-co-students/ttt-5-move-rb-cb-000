d4320c865d85181d8900d19e26fea51f0731e948
Bree Zuckerman <bz@bree.bz>
2017-04-07 20:41:09 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,19 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(input)  # Convert user input to an array index.
+  user_input = input
+  return input.to_i - 1 # Converts string to its integer value and subtracts 1 to account for array being 0-8.
+end
+
+# Your method must take in 3 arguments: the board array, the index in the board array that the player would like to fill out
+# with an "X" or an "O", and the player's character (either "X" or "O").
+# The third argument, the player's character, should have a default of "X".
+
+# #move should also return the modified array with the updated index corresponding to the player's token.
+# Don't create a new local variable for the board array, modify the one passed in as the argument and return it.
+# Print the updated board.
+
+def move(array, index, player="X")
+   array[index]=player
+end
