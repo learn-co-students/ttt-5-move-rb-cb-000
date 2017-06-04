a83ca0e736fc30765af063830f38ee5b09a214a0
Franklin Horn <franklin@whisper.sh>
2017-06-04 17:16:23 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,11 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(user_input)
+  intention = user_input.to_i
+  intention - 1
+end
+
+def move(board,intention,character="X")
+  board[intention] = character
+end
